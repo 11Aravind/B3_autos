fb92ef402c2b10f3b3f87077f36df27864f6ac38
@aravind49 <aravind@pixl.ai>
2025-06-21 11:31:50 +0530

    refactor ImageCarousel to use dynamic banners and improve structure; add banner images

--- src/Components/Carosal/ImageCarousel.jsx
+++ src/Components/Carosal/ImageCarousel.jsx
@@ -1,79 +1,53 @@
-// Import the Carousel styles
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { useState } from "react";
-
-
-// const ImageCarousel = () => {
-//     const [key, setKey] = useState(0);
-
-//     const handleChange = (index) => {
-//         if (index === /* last index */) {
-//             setKey((prevKey) => prevKey + 1);
-//         }
-//     };
-//     return(
-//     <Carousel
-//     key={key}
-//     showThumbs={false}
-//     showStatus={false}
-//     infiniteLoop
-//     autoPlay
-//     onChange={handleChange}
-//     interval={3000}
-//     transitionTime={600}
-//     >
-//         <div>
-//             <img src="images/banner.jpg" alt="Slide 1" />
-//             {/* <p className="legend">Slide 1</p> */}
-//         </div>
-//         <div>
-//             <img src="images/banner.jpg" alt="Slide 2" />
-//             {/* <p className="legend">Slide 2</p> */}
-//         </div>
-//         <div>
-//             <img src="images/banner.jpg" alt="Slide 3" />
-//             {/* <p className="legend">Slide 3</p> */}
-//         </div>
-//     </Carousel>
-// );
-// }
-
-// export default ImageCarousel;
+import banner1 from "../../assets/A1_banner.jpg";
+import banner2 from "../../assets/A2_banner.jpg"; 
+import banner3 from "../../assets/A3_banner.jpg";
+const banners = [
+  {
+    path: banner1,
+    alt: "Slider 1"
+  },
+  {
+    path: banner2,
+    alt: "Slider 2"
+  },
+  {
+    path: banner3,
+    alt: "Slider 3"
+  }
+];
 
 const ImageCarousel = () => {
   const [key, setKey] = useState(0);
 
   const handleChange = (index) => {
-    if (index === 3) {
-      setKey((prevKey) => prevKey + 1);
+    if (index === banners.length) {
+      setKey(prev => prev + 1);
     }
   };
 
+  const carouselSettings = {
+    key,
+    showThumbs: false,
+    showStatus: false,
+    infiniteLoop: true,
+    autoPlay: true,
+    onChange: handleChange,
+    interval: 3000,
+    transitionTime: 600
+  };
+
   return (
-    <Carousel
-      key={key}
-      showThumbs={false}
-      showStatus={false}
-      infiniteLoop
-      autoPlay
-      onChange={handleChange}
-      interval={3000}
-      transitionTime={600}
-    >
-      <div>
-        <img src="images/banner.jpg" alt="Slide 1" />
-        {/* <p className="legend">Slide 1</p> */}
-      </div>
-      <div>
-        <img src="images/banner.jpg" alt="Slide 2" />
-        {/* <p className="legend">Slide 2</p> */}
-      </div>
-      <div>
-        <img src="images/banner.jpg" alt="Slide 3" />
-        {/* <p className="legend">Slide 3</p> */}
-      </div>
+    <Carousel {...carouselSettings}>
+      {banners.map(({ path, alt }, index) => (
+        <div key={`slide-${index}`}>
+          <img src={path} alt={alt} loading="lazy" />
+        </div>
+      ))}
     </Carousel>
   );
 };
+
 export default ImageCarousel;
